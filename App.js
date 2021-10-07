cf99ab604d918102dd4f3e1eed63dc3c4fe96c80
Kim Brown <kbrownc341@gmail.com>
2021-10-07 10:26:54 -0400

    Last iteration with a DONE button

--- App.js
+++ App.js
@@ -16,7 +16,9 @@ const numRows = 8;
 
 // This has not been incorporated yet
 const wordLength = 3;
-console.log('********', Date());
+console.log('***************', Date());
+
+const newBoard2 = new Array(numColumns * numRows).fill('');
 
 const newBoard = [];
 let i;
@@ -25,15 +27,18 @@ for (i = 0; i < numColumns * numRows; i++) {
 }
 
 export default function App() {
-  const [{ message, score, board, endOfGame }, setGameState] = useState({
+  const [{ message, score, board, letterHistory }, setGameState] = useState({
     message: 'Enter 1st word',
     score: 0,
     board: JSON.parse(JSON.stringify(newBoard)),
-    endOfGame: false,
+    letterHistory: [],
   });
 
   // render board
   const renderBoard = ({ item }) => {
+    if (item.name !== '') {
+      console.log('RENDER Board',item.key - 1,item.name);
+    }
     return (
       <View style={styles.item}>
         <TextInput
@@ -51,11 +56,12 @@ export default function App() {
   // press Reset button
   const pressReset = useCallback(() => {
     setGameState(prevGameState => {
+      console.log('RESET');
       return {
         message: 'Reset Pressed',
         score: 0,
         board: JSON.parse(JSON.stringify(newBoard)),
-        endOfGame: false,
+        letterHistory: [],
       };
     });
   }, []);
@@ -63,13 +69,17 @@ export default function App() {
   // enter a Letter from keyboard
   const enterLetter = useCallback((value, key) => {
     setGameState(prevGameState => {
+      console.log('ENTERLETTER');
+      let workLetterHistory = JSON.parse(JSON.stringify(prevGameState.letterHistory));
       let workBoard = JSON.parse(JSON.stringify(prevGameState.board));
       let workMessage = 'Letter entered';
       workBoard[key - 1].name = value;
+      workLetterHistory.push(key - 1);
       return {
         ...prevGameState,
         message: workMessage,
         board: workBoard,
+        letterHistory: workLetterHistory,
       };
     });
   }, []);
@@ -78,13 +88,19 @@ export default function App() {
   //    Need to validate entry of letters (spelling and duplicates)
   //    Neeed to check if we are at end of game
   //    Need to update score
+  //
+  //  letterHistory - an array listing the letters entered onto the board in the order added
+  //   wordList - list of the 3-letter words on the board
   const pressDone = useCallback(() => {
     setGameState(prevGameState => {
+      console.log('DONE');
       let workMessage = 'Done button';
       let workBoard = JSON.parse(JSON.stringify(prevGameState.board));
-      let workEndOfGame = false;
+      let workLetterHistory = JSON.parse(JSON.stringify(prevGameState.letterHistory));
       let wordList = {};
+      let wordList2 = [];
       let word = '';
+      let wordIndex = 0;
       //  count the words on the board
       //  1) Count words on rows
       let i;
@@ -95,10 +111,16 @@ export default function App() {
             word = workBoard[i].name + workBoard[i + 1].name + workBoard[i + 2].name;
             if (wordList[word] === undefined) {
               wordList[word] = 1;
+              wordList2[wordList2.length] = {key: workBoard[i].key, name: workBoard[i].name};
+              wordList2[wordList2.length] = {key: workBoard[i + 1].key, name: workBoard[i + 1].name};
+              wordList2[wordList2.length] = {key: workBoard[i + 2].key, name: workBoard[i + 2].name};
             } else {
-              workBoard[i].name = '';
-              workBoard[i + 1].name = '';
-              workBoard[i + 2].name = '';
+              wordIndex = workLetterHistory[workLetterHistory.length - 1];
+              workBoard[wordIndex].name = '';
+              wordIndex = workLetterHistory[workLetterHistory.length - 2];
+              workBoard[wordIndex].name = '';
+              workLetterHistory.pop();
+              workLetterHistory.pop();
               workMessage = 'Duplicate word - word rejected';
             }
           }
@@ -118,25 +140,32 @@ export default function App() {
               workBoard[i * numColumns + j + numRows * 2].name;
             if (wordList[word] === undefined) {
               wordList[word] = 1;
+              wordList2[wordList2.length] = 
+                  {key: workBoard[i * numColumns + j].key, name: workBoard[i * numColumns + j].name};
+              wordList2[wordList2.length] = 
+                  {key: workBoard[i * numColumns + j + numRows].key, name: workBoard[i * numColumns + j + numRows].name};
+              wordList2[wordList2.length] = 
+                  {key: workBoard[i * numColumns + j + numRows * 2].key, name: workBoard[i * numColumns + j + numRows * 2].name};
             } else {
-              workBoard[i * numColumns + j].name = '';
-              workBoard[i * numColumns + j + numRows].name = '';
-              workBoard[i * numColumns + j + numRows * 2].name = '';
+              wordIndex = workLetterHistory[workLetterHistory.length - 1];
+              workBoard[wordIndex].name = '';
+              wordIndex = workLetterHistory[workLetterHistory.length - 2];
+              workBoard[wordIndex].name = '';
+              workLetterHistory.pop();
+              workLetterHistory.pop();
               workMessage = 'Duplicate word - word rejected';
             }
           }
         }
       }
-      // Check for end of Game
-      if (endOfGame) {
-        workMessage = 'Game Complete';
-      }
+      console.log('wordList',wordList);
+      console.log('wordList2',wordList2);
       return {
         ...prevGameState,
         message: workMessage,
         score: Object.keys(wordList).length,
         board: workBoard,
-        endOfGame: workEndOfGame,
+        letterHistory: workLetterHistory,
       };
     });
   }, []);
@@ -151,8 +180,8 @@ export default function App() {
       <View style={styles.container}>
         <View style={styles.nav}>
           <Button onPress={pressReset} title="Reset" color="blue" />
-          <View style={endOfGame ? styles.itemInvisible : null}>
-            <Button onPress={pressDone} title="Done" color="blue" disabled={endOfGame ? true : false} />
+          <View>
+            <Button onPress={pressDone} title="Done" color="blue" disabled={false} />
           </View>
           <View style={styles.itemNav}>
             <Text style={styles.itemText}>Score</Text>
